616ca1c7426fef4dcf4bb980e5cef8f859bf28f6
rahulrossi46 <2322ja35@kgcas.com>
2025-03-17 15:15:47 +0530

    DAY 1 CONCEPTS

---
+++ WEB DEVELOPMENT/DAY 1 JAVA TOPICS/src/basic/oops/Human.java
@@ -0,0 +1,18 @@
+package basic.oops;
+
+public class Human {
+	public void head() {
+			System.out.ptintln("Head function worked");
+	}
+	public void hand() {
+		System.out.println("Hand function worked");
+	}
+	
+	public void leg() {
+		System.out.println("Leg function worked");
+	}
+	public void run() {
+		System.out.println("Run function worked");
+	
+	}
+}
---
+++ WEB DEVELOPMENT/DAY 1 JAVA TOPICS/src/basic/oops/picil.java
@@ -0,0 +1,10 @@
+package basic.oops;
+
+public class picil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
